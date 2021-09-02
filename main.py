946f76fef578546f833010cee2a8ec3dd97ec589
behu <benedikt@diovision.net>
2021-09-02 18:06:37 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+import os
+
+
+def get_prefix(bot, message):
+    """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
+
+    # Notice how you can use spaces in prefixes. Try to keep them simple though.
+    prefixes = ["!"]
+
+    # Check to see if we are outside of a guild. e.g DM's etc.
+    if not message.guild:
+        # Only allow ? to be used in DMs
+        return '?'
+
+    # If we are in a guild, we allow for the user to mention us or use any of the prefixes in our list.
+    return commands.when_mentioned_or(*prefixes)(bot, message)
+
+
+class DiscordBot(commands.Bot):
+    def __init__(self, command_prefix, description):
+        super(DiscordBot, self).__init__(command_prefix=command_prefix,
+                                         description=description)
+
+    async def on_ready(self):
+        print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
+        print("Successfully logged in and booted...!")
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    token = os.getenv('DISCORD_TOKEN')
+    initial_extensions = ['music']
+    bot = DiscordBot(command_prefix=get_prefix, description='BenjaminBot')
+    for extension in initial_extensions:
+        bot.load_extension(extension)
+    bot.run(token, bot=True, reconnect=True)
---
+++ readme.md
@@ -0,0 +1,24 @@
+Discord Music Bot as replacement for Groovy.
+
+Mostly copy pasted from somewhere around the interwebs.
+
+!help
+```
+Music:
+  clear  Deletes entire queue of upcoming songs.
+  join   Connect to voice.
+  leave  Stop the currently playing song and destroy the player.
+  np     Display information about the currently playing song.
+  pause  Pause the currently playing song.
+  play   Request a song and add it to the queue.
+  queue  Retrieve a basic queue of upcoming songs.
+  remove Removes specified song from queue
+  resume Resume the currently paused song.
+  skip   Skip the song.
+  volume Change the player volume.
+No Category:
+  help   Shows this message
+
+Type !help command for more info on a command.
+You can also type !help category for more info on a category.
+```
